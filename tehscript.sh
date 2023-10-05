61771561626a0aa468a0cfe21aba222d31a0f86f
bunyon <40531275+dumpster-of-things@users.noreply.github.com>
2023-10-05 11:19:49 -0400

    Create tehscript.sh
    for mac

---
+++ tehscript.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+### Throw however many directories you'd like to check at this script (even if they themselves have spaces in their path (provided they are quoted))
+### Example:
+###         `./tehscript.sh /home/biden /opt "/home/other biden"
+
+That='https://raw.githubusercontent.com/browser-history/browser-history/master/browser_history/browsers.py'
+
+while read line ; do
+	for ((a=1;a<=$#;a++)); do [[ -d "${!a}" ]] || continue
+		[[ ! -e "${!a}/${line/*\ \=\ /}" ]] || echo "$line"
+	done
+done < <(curl -s "$That" | grep 'path =' browser_history_browsers.py 2>/dev/null )
+
+# curl ... | grep ... | cut -d\" -f2 | sed 's/ /\\ /g'
+
