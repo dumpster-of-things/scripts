c946e73b03c3f6d92a3cb5c114ce9e2f4f1dbb62
bunyon <40531275+dumpster-of-things@users.noreply.github.com>
2023-10-05 11:50:15 -0400

    Update tehscript.sh

--- tehscript.sh
+++ tehscript.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
 
 ### Throw however many directories you'd like to check at this script (even if they themselves have spaces in their path (provided they are quoted))
-### Example:
-###         `   ./tehscript.sh /home/biden /opt "/home/other biden"   `
+##  Example:
+#         `   ./tehscript.sh /home/biden /opt "/home/other biden"   `
 
-### if ran without arguments, reruns itself with your current directory as $1
-(($#!=0)) || { ${0} "${PWD}" ; exit $? ; }
+
+
+# if ran without arguments, re-runs itself with your current directory as $1
+(($#!=0)) || { ${0} "$PWD" ; exit $? ; }
+
 
 
 That='https://raw.githubusercontent.com/browser-history/browser-history/master/browser_history/browsers.py'
 
+
+# while looping over some lines parsed from "$That"
+#   (assuming said parsing returns some PATH(s) to look for)
 while read line ; do
+	#
+	# for any of them things you done said(: $@ ) that are directories
 	for ((a=1;a<=$#;a++)); do [[ -d "${!a}" ]] || continue
+		#
+		# say something if any of'm have said PATH(s) in there
  	 	[[ ! -e "${!a}/$line" ]] || echo "${!a}/$line"
+ 	 	#
+ 	 	# otherwise, do nothing.
 	done
 done < <(curl -s "$That" | grep 'path = ' | cut -d\" -f2 2>/dev/null )
 
